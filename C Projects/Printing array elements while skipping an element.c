c01c0bb70b8f637dcdf8328b2e7b3368d6b42895
Ahmed Al-Qasabi <ahmedalqasabi65@gmail.com>
2025-02-12 09:30:07 +0200

    C Projects

--- C Projects/Printing array elements while skipping an element.c
+++ C Projects/Printing array elements while skipping an element.c
@@ -1,3 +1,11 @@
+/*Hello World
+ * I am Eng.Ahmed Al-Qasabi
+ * Bachelor's degree in Mechatronics Engineering
+ * I specialized in embedded systems
+ * If you have any questions don't hesitate to communicate with me 😊😊
+ * https://www.linkedin.com/in/ahmed-al-qasabi-50a1292a4
+ */
+
 #include <stdio.h>
 
 void printArrayFromIndex(int arr[], int start, int size) {
